d6cfaa98d2537fac9668bf7d68adaa3826e9e5d9
Andrew Fleiger <afleiger@eagles.ewu.edu>
2018-10-02 14:48:21 -0700

    Assignment 2 Commit

---
+++ Assignment2/a2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Numerics;
+
+namespace SimpleMath
+{
+    class SimpleMath
+    {
+        static void Main(string[] args)
+        {
+            String input = Console.ReadLine();
+            if (input.Contains("+"))
+            {
+                String[] nums = input.Split("+");
+
+                BigInteger num1, num2;
+                if (BigInteger.TryParse(nums[0], out num1))
+                {
+                    if(BigInteger.TryParse(nums[1], out num2))
+                    {
+                        Console.WriteLine(num1 + num2);
+                    }
+                }
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Assignment2/a2/SimpleMathSolver.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace SimpleMathSolver
+{
+    class SimpleMathSolver
+    {
+        static void Main(string[] args)
+        {
+            double[] nums = null;
+            bool IsQuitting = false;
+            String output = "Formatting Error -- Please submit in {Value}{Operator}{Value} form.";
+
+            Console.WriteLine("----Math Solver----");
+
+            while (IsQuitting != true)
+            {
+                Console.WriteLine(Environment.NewLine + "Enter a problem ('Q' to quit): ");
+                String input = Console.ReadLine();
+                if (input.Trim().ToLower().Equals("q"))
+                {
+                    IsQuitting = true;
+                }
+
+                else if (input.Contains("+"))
+                {
+                    nums = ParseNumbers(input.Split("+"));
+                    if (nums != null)
+                    {
+                        output = (nums[0] + nums[1]).ToString();
+                    }
+                }
+
+                else if (input.Contains("-"))
+                {
+                    nums = ParseNumbers(input.Split("-"));
+                    if (nums != null)
+                    {
+                        output = (nums[0] - nums[1]).ToString();
+                    }
+                }
+
+                else if (input.Contains("*"))
+                {
+                    nums = ParseNumbers(input.Split("*"));
+                    if (nums != null)
+                    {
+                        output = (nums[0] * nums[1]).ToString();
+                    }
+                }
+
+                else if (input.Contains("/"))
+                {
+                    nums = ParseNumbers(input.Split("/"));
+                    if (nums != null)
+                    {
+                        if(nums[1] != 0)
+                        {
+                            output = (nums[0] / nums[1]).ToString();
+                        }
+                        else
+                        {
+                            output = "Error -- Divide by zero.";
+                        }
+                    }
+                }
+
+                Console.WriteLine($"Answer: {output}");
+            }
+
+
+        }
+
+        static double[] ParseNumbers(String[] stringArray)
+        {
+            double[] doubleArray = new double[2];
+            if (double.TryParse(stringArray[0].Trim(), out doubleArray[0]) && double.TryParse(stringArray[1].Trim(), out doubleArray[1]))
+            {
+                return doubleArray;
+            }
+            else return null;
+        }
+    }
+}
