dfbfaf078741d86c88f9514647d2d7c03afb83a3
Andrew Fleiger <afleiger@eagles.ewu.edu>
2018-10-04 15:38:34 -0700

    adding test files

---
+++ Assignment2/a2.test/MathSolverUnitTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace a2.test
+{
+    [TestClass]
+    public class MathSolverUnitTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
