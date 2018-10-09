626bdb7d7ee523333f5c0787218f84fdf7352c25
Andrew Fleiger <afleiger@eagles.ewu.edu>
2018-10-09 14:37:20 -0700

    Assignment 3 base commit

---
+++ Assignment3/Roshambo/roshambo.tests/Roshambo.Tests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace roshambo.tests
+{
+    [TestClass]
+    public class RoshamboUnitTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Assignment3/Roshambo/src/Roshambo.cs
@@ -0,0 +1,110 @@
+﻿using System;
+
+namespace Roshambo
+{
+    public class Roshambo
+    {
+        public static string newLine = Environment.NewLine;
+
+        public static void Main(string[] args)
+        {
+            int playerHealth = 100, compHealth = 100;
+            bool playAgain = true;
+
+            Console.WriteLine("----Roshambo----");
+            do
+            {
+                Console.WriteLine($"Player HP: {playerHealth}        Computer HP: {compHealth}");
+
+                (string player, string computer) round = PlayRound();
+
+                Console.WriteLine($"{newLine}You picked: {round.player}{newLine}Computer picked: {round.computer}");
+                playerHealth -= DetermineDamage(round.player, round.computer);
+                compHealth -= DetermineDamage(round.computer, round.player);
+
+                if (playerHealth < 1)
+                {
+                    Console.WriteLine($"{newLine}----YOU LOSE!!!----{newLine}Would you like to play again ('y' or 'n')?");
+                    playAgain = PromptForContinue();
+                    playerHealth = 100;
+                    compHealth = 100;
+                }
+                if (compHealth < 1)
+                {
+                    Console.WriteLine($"{newLine}----YOU WIN!!!----{newLine}Would you like to play again ('y' or 'n')?");
+                    playAgain = PromptForContinue();
+                    playerHealth = 100;
+                    compHealth = 100;
+                }
+            } while (playAgain);
+        }
+
+        public static (string player, string computer) PlayRound()
+        {
+            string player = PromptPlayer();
+            string computer = RandomChoice();
+            return (player, computer);
+        }
+
+        public static string PromptPlayer()
+        {
+            Console.WriteLine("Enter 'rock', 'paper', or 'scissors':");
+            return Console.ReadLine().ToLower();
+        }
+
+        public static string RandomChoice()
+        {
+            Random rand = new Random();
+            string ret = "rock";
+            switch (rand.Next(1, 4))
+            {
+                case 1:
+                    ret = "paper";
+                    break;
+                case 2:
+                    ret = "scissors";
+                    break;
+            }
+            return ret;
+        }
+
+        public static int DetermineDamage(string player, string opponent)
+        {
+            switch (player)
+            {
+                case "rock":
+                    if (opponent == "paper")
+                    {
+                        Console.WriteLine("-------------------------------------------->Paper covers Rock");
+                        return 10;
+                    }
+                    break;
+                case "paper":
+                    if (opponent == "scissors")
+                    {
+                        Console.WriteLine("-------------------------------------------->Scissors cuts Paper");
+                        return 15;
+                    }
+                    break;
+                case "scissors":
+                    if (opponent == "rock")
+                    {
+                        Console.WriteLine("-------------------------------------------->Rock crushes Scissors");
+                        return 20;
+                    }
+                    break;
+            }
+            return 0;
+        }
+
+        public static bool PromptForContinue()
+        {
+            switch(Console.ReadLine())
+            {
+                case "y":
+                    return true;
+            }
+            return false;
+        }
+    }
+}
