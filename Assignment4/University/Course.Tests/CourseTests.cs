d638af01833e80a4d6b4708510a1199496856f26
Andrew Fleiger <afleiger@eagles.ewu.edu>
2018-10-16 14:27:12 -0700

    Initial Commit - Created Event and Course Classes

---
+++ Assignment4/University/Course.Tests/CourseTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Course.Tests
+{
+    [TestClass]
+    public class CourseTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Assignment4/University/Course/Course.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Course
+{
+    public class Course
+    {
+        private string _lastName;
+        private string _firstName;
+        private int _studentCount;
+        private List<string> _days;
+
+        public int Id { get; }
+
+        public string Title { get; set; }
+
+        public decimal Tuition { get; set; }
+
+        public List<string> ClassDays { get; set; }
+
+        public decimal TotalRevenue
+        {
+            get
+            {
+                return Tuition * StudentCount;
+            }
+        }
+
+        public int StudentCount
+        {
+            get
+            {
+                return _studentCount;
+            }
+            set
+            {
+                if (value >= 0)
+                {
+                    _studentCount = value;
+                }
+            }
+        }
+
+        public string InstructorName
+        {
+            get
+            {
+                return $"{_lastName}, {_firstName}";
+            }
+            set
+            {
+                string[] splitName = value.Split(' ');
+                if (!string.IsNullOrEmpty(splitName[0]) && !string.IsNullOrEmpty(splitName[1]))
+                {
+                    _firstName = splitName[0];
+                    _lastName = splitName[1];
+                }
+            }
+        }
+
+    }
+}
