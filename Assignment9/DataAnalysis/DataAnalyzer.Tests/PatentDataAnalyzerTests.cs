3aad30d510e496cdfdbef6c6360f7d9c5c3b4b0e
Fleiger <afleiger@eagles.ewu.edu>
2018-12-03 22:14:56 -0800

    LINQ Assignment

---
+++ Assignment9/DataAnalysis/DataAnalyzer.Tests/PatentDataAnalyzerTests.cs
@@ -0,0 +1,48 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DataAnalyzer.Tests
+{
+    [TestClass]
+    public class PatentDataAnalyzerTests
+    {
+        [TestMethod]
+        public void InventorNames_QueryForUK_GetProperResultSuccess()
+        {
+            List<string> list = PatentDataAnalyzer.InventorNames("UK");
+
+            Assert.AreEqual("George Stephenson", list[0]);
+        }
+
+        [TestMethod]
+        public void InventorLastNames_CallMethod_GetListOfLastNamesSortedCorrectly()
+        {
+            List<string> lastNames = PatentDataAnalyzer.InventorLastNames();
+
+            Assert.AreEqual("Jacob", lastNames[0]);
+            Assert.AreEqual("Franklin", lastNames[6]);
+        }
+
+        [TestMethod]
+        public void LocationsWithInventors_CallMethod_Success()
+        {
+            string locations = PatentDataAnalyzer.LocationsWithInventors();
+
+            Assert.AreEqual("PA-USA,NC-USA,NY-USA,Northumberland-UK,IL-USA", locations);
+        }
+
+        [TestMethod]
+        public void Randomize()
+        {
+            List<string> list = PatentDataAnalyzer.InventorLastNames().ToList();
+            List<string> randomizedList = list.Randomize().ToList();
+            List<string> randomizedList2 = list.Randomize().ToList();
+            List<string> randomizedList3 = list.Randomize().ToList();
+
+            Assert.IsFalse(list.SequenceEqual(randomizedList));
+            Assert.IsFalse(list.SequenceEqual(randomizedList2));
+            Assert.IsFalse(list.SequenceEqual(randomizedList3));
+        }
+    }
+}
---
+++ Assignment9/DataAnalysis/DataAnalyzer/PatentDataAnalyzer.cs
@@ -0,0 +1,56 @@
+﻿using AddisonWesley.Michaelis.EssentialCSharp.Chapter15.Listing15_10;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace DataAnalyzer
+{
+    public static class PatentDataAnalyzer
+    {
+        public static List<string> InventorNames(string country)
+        {
+            List<string> result = PatentData.Inventors
+                .Where(i => i.Country.Equals(country))
+                .Select(i => i.Name).ToList();
+
+            return result;
+        }
+
+        public static List<string> InventorLastNames()
+        {
+            List<string> result = PatentData.Inventors
+                .OrderByDescending(i => i.Id)
+                .Select(i => i.Name.Split().Last()).ToList();
+
+            return result;
+        }
+
+        public static string LocationsWithInventors()
+        {
+            string result = string.Join(',', PatentData.Inventors
+                .Select(i => $"{i.State}-{i.Country}")
+                .Distinct());
+
+            return result;
+        }
+    }
+
+    public static class Enumerable
+    {
+        public static IEnumerable<T> Randomize<T>(this IEnumerable<T> enumerable)
+        {
+            List<T> original = enumerable.ToList();
+            Random rand = new Random();
+
+            while(original.Any())
+            {
+                int nextIndex = rand.Next(0, original.Count);
+                T item = original[nextIndex];
+                original.RemoveAt(nextIndex);
+
+                yield return item;
+            }
+        }
+    }
+}
